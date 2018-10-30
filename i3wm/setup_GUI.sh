f71c1f2a7e8774b4907e1c8b57d019d84a8c2bbe
puppy <puppy@dog.lan>
2018-10-30 17:34:03 +0300

    Removed commented old code in setting up GUI script

--- i3wm/setup_GUI.sh
+++ i3wm/setup_GUI.sh
@@ -13,22 +13,6 @@ if [ -z ${SUDO_USER} ]; then
     echo -e "\n\x1b[31;01m No \$SUDO_USER available, quiting ... \x1b[39;49;00m\n"
     exit 1
 fi
-# run apt-get update
-# if [ "$(( $(date +%s) - $(stat -c %Z /var/cache/apt/pkgcache.bin 2>/dev/null || echo '0') ))" -ge 600 ]; then
-#     echo -e "\n\x1b[33;01m Running apt-get update ... \x1b[39;49;00m\n" && sleep 1
-#     apt-get update
-# fi
-
-# install packages
-# apt-get install -y vim-nox curl git wgepython-pip exuberant-ctags
-# if hash nvim 2>/dev/null; then
-#     echo -e "\n\x1b[33;01m neovim is installed, not installing or upgrading. \x1b[39;49;00m\n" && sleep 1
-# else
-#     echo -e "\n\x1b[33;01m Installing neovim ...  \x1b[39;49;00m\n" && sleep 1
-#     # TODO: python-neovim python3-neovim are available only in 'unstable' deb
-#     # repository
-#     apt-get install -y neovim curl python-neovim python3-neovim
-# fi
 
 # link .XResources file
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
