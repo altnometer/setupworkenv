efadf2e88b01721d11ed8af22ebbe63f7105e8a2
puppy <puppy@dog.lan>
2018-11-10 18:47:17 +0300

    Changed inactive session format in tmux status bar

--- tmuxsetup/status_bar_left.sh
+++ tmuxsetup/status_bar_left.sh
@@ -2,6 +2,7 @@
 tmux ls -F "#{?session_attached,\
 #[fg=red]#S:#[fg=yellow]#I#[fg=colour58](#{session_windows})\
 #[fg=cyan]#P#[fg=colour23](#{window_panes}),\
-#[fg=colour23]#S#[fg=colour58](#{session_windows})"\
+#[fg=colour58]#S"\
 }\
 | tr "\\n" " "
+#[fg=colour23]#S#[fg=colour58](#{session_windows})"\
