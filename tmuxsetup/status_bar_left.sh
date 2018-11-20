bc662b78f8456ace7ab54f672e019643ee3b6fb0
puppy <puppy@dog.lan>
2018-11-20 09:42:51 +0300

    Changed tmux status bar active session to colour3

--- tmuxsetup/status_bar_left.sh
+++ tmuxsetup/status_bar_left.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux ls -F "#{?session_attached,\
-#[fg=red]#S:#[fg=yellow]#I#[fg=colour58](#{session_windows})\
+#[fg=colour3]#S#[fg=colour58]:#[fg=yellow]#I#[fg=colour58](#{session_windows})\
 #[fg=cyan]#P#[fg=colour23](#{window_panes}),\
 #[fg=colour58]#S"\
 }\
