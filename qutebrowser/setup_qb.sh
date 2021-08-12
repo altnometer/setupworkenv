6e358fd2e111b5d741932d8e86d37d151cb86763
sam at lf <sam\@lf>
2021-08-12 09:56:55 +0300

    qutebrowser: add linking backup quickmarks

--- qutebrowser/setup_qb.sh
+++ qutebrowser/setup_qb.sh
@@ -2,6 +2,7 @@
 
 # This scrip should set up qutebrowser.
 
+# Check if the scrip is run with 'sudo -E'
 if [[ $EUID -ne 0 ]]; then
     echo -e "\n\x1b[31;01m Run this script with 'sudo -E' \x1b[39;49;00m\n"
     exit 1
@@ -14,6 +15,7 @@ if [ -z ${SUDO_USER} ]; then
     echo -e "\n\x1b[31;01m No \$SUDO_USER available, quiting ... \x1b[39;49;00m\n"
     exit 1
 fi
+
 SCRIPT_DIR_OLD=$SCRIPT_DIR
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DEST_DIR="${HOME}/.config/qutebrowser"
@@ -45,5 +47,26 @@ else
     echo -e "\n\x1b[31;01m $CONF_SOURCE_PATH does not exist. Quiting ... \x1b[39;49;00m\n"
 	exit 1
 fi
+
+# ** link quickmarks
+
+QUICKMARKS_SOURCE_PATH="${BACKUP_DIR}/qutebrowser/quickmarks"
+QUICKMARKS_DEST_PATH="${HOME}/.config/qutebrowser/quickmarks"
+
+if [ -f $QUICKMARKS_SOURCE_PATH ];
+then
+    echo -e "\n\x1b[33;01m Linking $QUICKMARKS_SOURCE_PATH to $QUICKMARKS_DEST_PATH ... \x1b[39;49;00m\n"
+    if [ -f "$QUICKMARKS_DEST_PATH" ]; then
+        rm $QUICKMARKS_DEST_PATH
+    fi
+    if [ -h "$QUICKMARKS_DEST_PATH" ]; then  # -h, true if file exist and a symbolic link.
+        rm $QUICKMARKS_DEST_PATH
+    fi
+    sudo -u ${SUDO_USER} ln -s $QUICKMARKS_SOURCE_PATH $QUICKMARKS_DEST_PATH
+else
+    echo -e "\n\x1b[31;01m $QUICKMARKS_SOURCE_PATH does not exist. Quiting ... \x1b[39;49;00m\n"
+	exit 1
+fi
+
 SCRIPT_DIR=$SCRIPT_DIR_OLD
 # }}}
