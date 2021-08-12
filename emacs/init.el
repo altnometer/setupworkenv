db732ab1063ac5d9bb4e12125f892cfd09e7615b
sam at lf <sam\@lf>
2021-08-12 15:49:54 +0300

    emacs/conf/ocaml: comment out (require 'opam-user-setup ...)

--- emacs/init.el
+++ emacs/init.el
@@ -198,5 +198,5 @@
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'erase-buffer 'disabled nil)
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
