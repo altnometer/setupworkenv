70563bb9fd9f3de64a3183a0dd46628fc901cd71
sam at lf <sam\@lf>
2021-08-14 08:45:20 +0300

    emacs/conf: add binding to #'vterm-copy-mode #'vterm-copy-mode-done

--- emacs/config/packages.el
+++ emacs/config/packages.el
@@ -248,6 +248,8 @@
  '(vterm :type git :flavor melpa :files
          ("*" (:exclude ".dir-locals.el" ".gitignore" ".clang-format" ".travis.yml") "vterm-pkg.el")
          :host github :repo "akermu/emacs-libvterm"))
+(define-key vterm-mode-map (kbd "M-<f9>") #'vterm-copy-mode)
+(define-key vterm-copy-mode-map (kbd "M-<f9>") #'vterm-copy-mode-done)
 (require 'vterm)
 
 ;;* eshell
