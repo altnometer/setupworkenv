fd90486cf569c713d700509c52b37c7a6ab639a0
sam at lf <sam\@lf>
2021-10-18 11:44:15 +0300

    emacs/org-roam: enable updating 'LAST-MODIFIED' property

--- emacs/config/packages.el
+++ emacs/config/packages.el
@@ -1852,6 +1852,57 @@ displaying TEST-BUFFER-P buffer."
 
 ;;** org-mode: functions
 
+;; credit to https://github.com/zaeph/.emacs.d/blob/4548c34d1965f4732d5df1f56134dc36b58f6577/init.el
+(defun zp/org-find-time-file-property (property &optional anywhere)
+  "Return the position of the time file PROPERTY if it exists.
+
+When ANYWHERE is non-nil, search beyond the preamble."
+  (save-excursion
+    (goto-char (point-min))
+    (let ((first-heading
+           (save-excursion
+             (re-search-forward org-outline-regexp-bol nil t))))
+      (when (re-search-forward (format "^#\\+%s:" property)
+                               (if anywhere nil first-heading)
+                               t)
+        (point)))))
+
+(defun zp/org-has-time-file-property-p (property &optional anywhere)
+  "Return the position of time file PROPERTY if it is defined.
+
+As a special case, return -1 if the time file PROPERTY exists but
+is not defined."
+  (when-let ((pos (zp/org-find-time-file-property property anywhere)))
+    (save-excursion
+      (goto-char pos)
+      (if (and (looking-at-p " ")
+               (progn (forward-char)
+                      (org-at-timestamp-p 'lax)))
+          pos
+        -1))))
+(defun zp/org-set-time-file-property (property &optional anywhere pos)
+  "Set the time file PROPERTY in the preamble.
+
+When ANYWHERE is non-nil, search beyond the preamble.
+
+If the position of the file PROPERTY has already been computed,
+it can be passed in POS."
+  (when-let ((pos (or pos
+                      (zp/org-find-time-file-property property))))
+    (save-excursion
+      (goto-char pos)
+      (if (looking-at-p " ")
+          (forward-char)
+        (insert " "))
+      (delete-region (point) (line-end-position))
+      (let* ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
+        (insert now)))))
+
+(defun zp/org-set-last-modified ()
+  "Update the LAST_MODIFIED file property in the preamble."
+  (when (derived-mode-p 'org-mode)
+    (zp/org-set-time-file-property "LAST_MODIFIED")))
+
 (defun ram-hide-block-toggle ()
   "Toggle visibility from anywhere in the block."
   (interactive)
@@ -1864,7 +1915,6 @@ displaying TEST-BUFFER-P buffer."
 
 (with-eval-after-load "org"
   (define-key org-mode-map (kbd "<C-tab>") #'ram-hide-block-toggle))
-
 
 ;;** org-mode: bindings
 
@@ -1920,6 +1970,8 @@ displaying TEST-BUFFER-P buffer."
 (add-hook 'org-mode-hook 'org-indent-mode)
 (add-hook 'org-mode-hook 'org-hide-block-all)
 (add-hook 'org-mode-hook (lambda () (variable-pitch-mode t)))
+;; credit to https://github.com/zaeph/.emacs.d/blob/4548c34d1965f4732d5df1f56134dc36b58f6577/init.el
+(add-hook 'org-mode-hook #'zp/org-set-last-modified)
 
 ;;** org-mode: faces, fonts
 
